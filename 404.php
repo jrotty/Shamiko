a944b2155188e498a6d12f2d068fdbf99ca24402
jrotty <bssf@qq.com>
2019-09-02 18:40:21 +0800

    404页面
    404页面：作者上传项目时才发现忘记写我了，我好难过！然后作者就随便几行代码应付了我，我就那么不值得重视吗QAQ！

---
+++ 404.php
@@ -0,0 +1,24 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+ $this->need('header.php');
+ ?>
+
+<div class="container pb-10">
+<div class="row">
+<div class="col-sm-12"><div class="card post">
+<h1>404</h1>
+
+
+
+<div class="wysiwyg">你访问的页面不存在或管理员已删除！</div>
+</div></div>
+
+
+
+
+
+
+</div></div>
+
+
+
+<?php $this->need('footer.php'); ?>
